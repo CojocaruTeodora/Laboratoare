981509041365f2fb0102b6d15d8a5f6d2c953d1a
CojocaruTeodora <74421666+CojocaruTeodora@users.noreply.github.com>
2022-05-21 11:28:11 +0300

    Add files via upload

---
+++ Exemplu 3.js
@@ -0,0 +1,47 @@
+//exercitiul 3
+let styles = ["Jazz", "Blues"];
+console.log(styles);
+/*styles.push("Rock and Roll");
+console.log(styles);
+styles[1] = "Clasica"
+console.log(styles);
+alert(styles.shift());
+console.log(styles);
+styles.unshift("Rap", "Reggy");
+console.log(styles);*/
+
+//Exercitiul 1
+//let numar = ["1",'2',"3","4","5"];
+/*for (let i = 0; i < numar.length; i++) {
+    console.log(numar);
+}*/
+/*for (let numere of numar){
+    console.log(numar);
+}*/
+//exercitiul 4
+/*function sumInput() {
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("Introduceti", 0);
+  
+      
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+  alert( sumInput() );*/
+  //exercitiul 6
+/*let numar = [1, 2, 3, 4, 5, 6, 7, 8, 9];  
+numar.splice(4,1, "0", "0","0");
+console.log(numar);*/
+
